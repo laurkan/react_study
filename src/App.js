95ff30b6ece35713e1ed9d50d0d7ee03567c70b1
laurkan <kanya384@yandex.ru>
2020-01-29 14:30:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+let i=0
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>{++i}. Элемент 1</li>
+        <li>{++i}. Элемент 2</li>
+      </ul>
+      <img src={logo} />
     </div>
   );
 }
